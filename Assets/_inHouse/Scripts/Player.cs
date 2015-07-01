709b3ad0fb0dcd88a59d293fb3e19e0ef1fc5744
Doug Seldeen <gamedev@duggo42.com>
2015-07-01 01:07:32 -0700

    Added basic properties for Player.cs

--- Assets/_inHouse/Scripts/Player.cs
+++ Assets/_inHouse/Scripts/Player.cs
@@ -1,5 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+public class Player {
+	[System.Flags]
+	public enum Position {
+		PG = 1,
+		SG = 2,
+		SF = 4,
+		PF = 8,
+		C = 16
+	}
+
+	public int playerID;
+
+	public Position position;
+
+	public int uniformNumber;
+
+	public int offenseRating;
+	public int defenseRating;
+	public int reboundRating;
+	public int ballHandlingRating;
+	public int freeThrowRating;
+
 }
